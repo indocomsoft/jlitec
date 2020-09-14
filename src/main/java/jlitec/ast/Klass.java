22added267aab1a6f89b6c522b18f7ed81dc34f9
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-09-15 00:07:50 +0800

    Finish up parser

--- src/main/java/jlitec/ast/Klass.java
+++ src/main/java/jlitec/ast/Klass.java
@@ -2,7 +2,7 @@ package jlitec.ast;
 
 import java.util.List;
 
-public class Class {
+public class Klass {
   public final String cname;
   public final List<Var> fields;
   public final List<Method> methods;
@@ -14,7 +14,7 @@ public class Class {
    * @param fields list of class fields.
    * @param methods list of class methods.
    */
-  public Class(String cname, List<Var> fields, List<Method> methods) {
+  public Klass(String cname, List<Var> fields, List<Method> methods) {
     this.cname = cname;
     this.fields = fields;
     this.methods = methods;
--- src/main/java/jlitec/ast/Program.java
+++ src/main/java/jlitec/ast/Program.java
@@ -3,9 +3,9 @@ package jlitec.ast;
 import java.util.List;
 
 public class Program {
-  public final List<Class> classList;
+  public final List<Klass> klassList;
 
-  public Program(List<Class> classList) {
-    this.classList = classList;
+  public Program(List<Klass> klassList) {
+    this.klassList = klassList;
   }
 }
