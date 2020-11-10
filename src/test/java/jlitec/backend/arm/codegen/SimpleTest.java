61db85419d47aee22e779b62b5b4ad9606ec58a9
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-11-10 19:56:48 +0800

    Add tests for Simple ARM backend

---
+++ src/test/java/jlitec/backend/arm/codegen/SimpleTest.java
@@ -0,0 +1,29 @@
+package jlitec.backend.arm.codegen;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import jlitec.checker.ParseTreeStaticChecker;
+import jlitec.ir3.codegen.Ir3CodeGen;
+import jlitec.parser.ParserWrapper;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class SimpleTest {
+  @ParameterizedTest
+  @MethodSource("loadTests")
+  void test(String filename) throws Exception {
+    final var program = new ParserWrapper(filename).parse();
+    final var klassDescriptorMap = ParseTreeStaticChecker.produceClassDescriptor(program);
+    final var astProgram = ParseTreeStaticChecker.toAst(program, klassDescriptorMap);
+    final var ir3Program = Ir3CodeGen.generate(astProgram);
+    assertDoesNotThrow(() -> Simple.gen(ir3Program).print(0));
+  }
+
+  private static Stream<String> loadTests() throws IOException {
+    return Files.list(Paths.get("test", "arm")).map(f -> f.toAbsolutePath().toString());
+  }
+}
