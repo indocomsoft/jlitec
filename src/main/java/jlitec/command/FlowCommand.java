619b12b412902b6c6109cce72b16b277f4cd504c
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-11-20 09:56:45 +0800

    Add --opt to FlowCommand

--- src/main/java/jlitec/command/FlowCommand.java
+++ src/main/java/jlitec/command/FlowCommand.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
+import jlitec.backend.passes.PassManager;
 import jlitec.backend.passes.flow.FlowPass;
 import jlitec.backend.passes.lower.LowerPass;
 import jlitec.checker.KlassDescriptor;
@@ -13,6 +14,7 @@ import jlitec.ir3.codegen.Ir3CodeGen;
 import jlitec.lexer.LexException;
 import jlitec.parser.ParserWrapper;
 import jlitec.parsetree.Program;
+import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
 
@@ -25,6 +27,10 @@ public class FlowCommand implements Command {
   @Override
   public void setUpArguments(Subparser subparser) {
     subparser.addArgument("filename").type(String.class).help("input filename");
+    subparser
+        .addArgument("--opt")
+        .help("Perform optimization passes")
+        .action(Arguments.storeTrue());
     subparser
         .addArgument("--output-dir", "-o")
         .type(String.class)
@@ -72,9 +78,12 @@ public class FlowCommand implements Command {
       return;
     }
 
+    final var opt = parsed.getBoolean("opt");
     final jlitec.ir3.Program ir3Program = Ir3CodeGen.generate(astProgram);
     final var lowerProgram = new LowerPass().pass(ir3Program);
-    for (final var method : lowerProgram.methodList()) {
+    final var finalProgram =
+        opt ? PassManager.performOptimizationPasses(lowerProgram) : lowerProgram;
+    for (final var method : finalProgram.methodList()) {
       final var methodName = method.id();
       final var flow = new FlowPass().pass(method.lowerStmtList());
       final var path = Paths.get(outputDir, methodName + ".dot");
